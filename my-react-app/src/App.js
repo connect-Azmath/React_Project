c3af9c7a61452dcdabe9691c97ac6e3ef3cfbf6a
connect-Azmath <122971335+connect-Azmath@users.noreply.github.com>
2023-08-22 15:00:33 +0000

    Updated componets

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,15 +2,20 @@
 // import './App.css';
 import ClassComponet from "./components/ClassComponent";
 import FunctionalComponent from "./components/FunctionalComponent";
+import { useState } from "react";
 
 function App() {
+  const company = "Az Groups with me"
+  const [name, setName] = useState("Azmath");
+
   return (
     <div className="App">
       <header className="App-header">
       <h1> Hi Wolrd, I am from React </h1>
       <ClassComponet /> 
-      <FunctionalComponent name = "Azmath" dept = "CS" id = {10} />
-     </header>
+      <FunctionalComponent name = {name} dept = "CS" id = {10} setName = {setName} />
+      {/* <FunctionalComponent name = "Azmath" dept = "CS" id = {10} /> */}
+      </header>
     </div>
   );
 }
--- my-react-app/src/components/FunctionalComponent.jsx
+++ my-react-app/src/components/FunctionalComponent.jsx
@@ -3,7 +3,13 @@ import { useState } from "react";
 
 const FunctionalComponent = (props) => {
     const [count, setCount] = useState(0);
+    const [changeName, setChangeName] = useState("");
     const subtract = () => setCount(count - 1)
+    const [name, setName] = useState("")
+    
+    // const [name, age, company, setName] = props; 
+    // By assigning variable to props we can remove props.name etc to just {name}
+
     return(
         <div>
             <p> Funcitonal Component</p>
@@ -12,6 +18,9 @@ const FunctionalComponent = (props) => {
             <button onClick={subtract}>Click on me to subtract -1 </button>
          <h1>{count}</h1>
          <h2>My name is {props.name}, I am from {props.dept} dept and my ID is {props.id}</h2>
+        <input type="text" onChange={(e) => setChangeName(e.target.value)}/>
+        <button onClick= {() => props.setName("Azmathulla")}> Change Name to Azmathulla</button>
+        <button onClick = {() => props.setName(changeName)}>Change Name</button>
         </div>
     )
 }
