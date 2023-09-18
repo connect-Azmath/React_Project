2197175893cb2d99df5fab2e1027e65bd628b3ee
connect-Azmath <122971335+connect-Azmath@users.noreply.github.com>
2023-09-18 15:53:45 +0000

    final code

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -12,19 +12,19 @@ function App() {
 
   return (
 
-    // <div className="App">
-    //   <header className="App-header">
-    //   <h1> Hi Wolrd, I am from React </h1>
-    //   <ClassComponet /> 
-    //   <FunctionalComponent name = {name} dept = "CS" id = {10} setName = {setName} />
-    //   <FunctionalComponent name = {name} dept = {dept} id = {10}  setDept = {setDept} /> 
-    //   </header>
-    // </div>
+    <div className="App">
+      <header className="App-header">
+      <h1> Hi Wolrd, I am from React </h1>
+      <ClassComponet /> 
+      <FunctionalComponent name = {name} dept = "CS" id = {10} setName = {setName} />
+      {/* <FunctionalComponent name = {name} dept = {dept} id = {10}  setDept = {setDept} />  */}
+      </header>
+    </div>
 
-    <Routes>
-      <Route path="/" element={<ClassComponet />}></Route>
-      <Route path="/functional-component" element={<FunctionalComponent />}></Route>  
-     </Routes>
+    // <Routes>
+    //   <Route path="/" element={<ClassComponet />}></Route>
+    //   <Route path="/functional-component" element={<FunctionalComponent />}></Route>  
+    //  </Routes>
 
 
   );
